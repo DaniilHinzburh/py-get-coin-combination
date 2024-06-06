cec477aca3c14ab9e311e9ad3133b5974d6ddb83
ilgan <2ilgan1601@gmail.com>
2024-06-06 18:00:21 +0300

    1

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_get_coin_combination_check_penny():
+    assert get_coin_combination(1)[0] == 1
+
+
+def test_get_coin_combination_check_nickel():
+    assert get_coin_combination(5)[1] == 1
+
+
+def test_get_coin_combination_check_dime():
+    assert get_coin_combination(10)[2] == 1
+
+
+def test_get_coin_combination_check_quarters():
+    assert get_coin_combination(25)[3] == 1
+
+
+def test_get_coin_combination_check_all():
+    assert get_coin_combination(41) == [1, 1, 1, 1]
